f757048b50eab760d30a633da53776e62fd9297c
Mohd Farhanullah <mfu7379@gmail.com>
2024-01-18 07:09:24 -0800

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,11 @@
-import { SearchOffOutlined } from "@mui/icons-material";
+
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
     return (<>
       
     <h1>Hello, Dashboard Page!</h1>
-    <div><SearchOffOutlined/></div>
+    <div></div>
     </>
     )
   }
