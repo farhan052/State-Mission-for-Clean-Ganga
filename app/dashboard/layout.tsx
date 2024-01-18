0d3b96dff4e47b7bd3fc674ad7ab42633666a461
Mohd Farhanullah <mfu7379@gmail.com>
2024-01-18 06:37:57 -0800

    test

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+
+
+
+export default function DashboardLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section>
+        
+        
+      
+        <div>
+  
+        {children}
+        </div>
+      </section>
+    )
+  }
+ 
