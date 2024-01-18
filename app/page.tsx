a336708997e4b714e6eb2ebe5993935733da5bed
Mohd Farhanullah <mfu7379@gmail.com>
2024-01-18 09:24:52 -0800

    n

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     
       <Link href="/dashboard">Dashboard</Link>
       <div className='grid bg-slate-200 border-spacing-3'>
-       <h3 className='my-3 h3 bg-white text-2xl mx-auto'>{count}</h3>
+       <h3 className='my-3 h3 text-2xl mx-auto'>{count}</h3>
        <div className='flex gap-2'>
         <div className='mx-1 bg-red-400 p-2 font-bold'><button onClick={()=> setcount(count-1)} >- Decrement</button></div>
         <div><button className='bg-blue-100 p-2  font-bold' onClick={()=>setcount(count+1)}>+ Increment</button></div>
